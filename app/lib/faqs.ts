1439c9d36d4216aa48b33507173830a69d4a8486
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-08-19 16:12:17 -0300

    feat: add Faq comoponent

---
+++ app/lib/faqs.ts
@@ -0,0 +1,32 @@
+export const listfaqs = [
+   {
+      id: 1,
+      question: "Pellentesque ac bibendum tortor?",
+      answer:
+         "Vivamus sit amet interdum elit. Proin lacinia erat ac velit tempus auctor. ",
+   },
+   {
+      id: 2,
+      question: "Pellentesque ac bibendum tortor?",
+      answer:
+         "Vivamus sit amet interdum elit. Proin lacinia erat ac velit tempus auctor. ",
+   },
+   {
+      id: 3,
+      question: "Pellentesque ac bibendum tortor?",
+      answer:
+         "Vivamus sit amet interdum elit. Proin lacinia erat ac velit tempus auctor. ",
+   },
+   {
+      id: 4,
+      question: "Pellentesque ac bibendum tortor?",
+      answer:
+         "Vivamus sit amet interdum elit. Proin lacinia erat ac velit tempus auctor. ",
+   },
+   {
+      id: 5,
+      question: "Pellentesque ac bibendum tortor?",
+      answer:
+         "Vivamus sit amet interdum elit. Proin lacinia erat ac velit tempus auctor. ",
+   },
+];
