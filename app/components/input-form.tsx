b748f47f6a1dff1260971824973510e394a2146d
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-08-19 16:09:15 -0300

    feat: add `input-form` component

---
+++ app/components/input-form.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { InputHTMLAttributes } from "react";
+
+interface InputFormProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export default function InputForm({ ...rest }: InputFormProps) {
+   return (
+      <label className="flex flex-row items-center rounded-md border border-black/50 px-2 ring-[#F7A932] ring-offset-2 focus-within:border-none focus-within:ring-1">
+         <input
+            {...rest}
+            className="h-10 w-[17rem] rounded-md bg-transparent px-5 outline-none"
+         />
+      </label>
+   );
+}
