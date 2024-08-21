f30be6dc1e0830907728ce29fc4ef732189c8109
Helcio <helcioanicio19@gmail.com>
2024-08-21 15:15:25 -0300

    feat(price): updated Price screen with component created

--- app/prices/page.tsx
+++ app/prices/page.tsx
@@ -1,116 +1,75 @@
 import Header from "../components/header";
 import { IoShieldCheckmark } from "react-icons/io5";
-import Button from "./Components/button";
+import Footer from "../components/footer";
+import Faq from "../components/faq";
+import { Card } from "./Components/card";
 
 const PricePage = () => {
+   const faqData = [
+      {
+         id: 1,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+      {
+         id: 2,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+      {
+         id: 3,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+      {
+         id: 4,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+      {
+         id: 5,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+   ];
+
    return (
       <div className="font-sans">
-         <section className="relative z-10 h-[420px] w-full bg-bg-header bg-contain bg-no-repeat">
+         <section className="h-56 w-full bg-wave-header bg-cover bg-bottom bg-no-repeat text-white">
             <Header />
          </section>
-         <section className="flex flex-col items-center gap-10 bg-white">
+         <section className="-mt-20 flex flex-col items-center gap-10 bg-white">
             <div className="flex flex-col items-center gap-3">
                <p>
-                  <IoShieldCheckmark className="size-14 text-tertiary" />
+                  <IoShieldCheckmark className="text-14 text-tertiary" />
                </p>
                <h1 className="w-full max-w-52 text-center text-3xl font-bold">
                   Comparativo dos planos
                </h1>
             </div>
 
-            <article className="w-full max-w-xs rounded-md border shadow-rounded">
-               <div className="flex flex-col items-center border-b border-tertiary p-2">
-                  <h3 className="font-bold">Basico</h3>
-                  <p className="font-bold">$0</p>
-                  <p className="text-xs font-bold text-slate-400">por month</p>
+            <Card color="white" price={0} plan="Basico" />
+            <Card color="blue" price={0} plan="Basico" />
+            <Card color="white" price={0} plan="Basico" />
+            <div className="mt-10 flex flex-col items-center gap-20">
+               <h4 className="text-2xl font-medium uppercase text-gray-400">
+                  Dúvidas frequentes
+               </h4>
+               <span className="h-1 w-1/2 bg-amber-400"> </span>
 
-                  <Button />
-               </div>
-
-               <div className="py-6">
-                  <ul className="flex flex-col gap-2">
-                     <li className="bg-orange-200 py-2 pl-2 text-xs font-light text-gray-900">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit.
-                     </li>
-                     <li className="bg-orange-200 py-2 pl-2 text-xs font-light text-gray-900">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit.
-                     </li>
-                     <li className="bg-orange-200 py-2 pl-2 text-xs font-light text-gray-900">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit.
-                     </li>
-                     <li className="bg-orange-200 py-2 pl-2 text-xs font-light text-gray-900">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit.
-                     </li>
-                  </ul>
-               </div>
-               <button className="text-xs font-bold text-slate-400">
-                  Mostrar menos
-               </button>
-            </article>
-
-            <article className="w-full max-w-xs rounded-md border shadow-rounded">
-               <div className="flex flex-col items-center border-b border-tertiary p-2">
-                  <h3 className="font-bold">Basico</h3>
-                  <p className="font-bold">$0</p>
-                  <p className="text-xs font-bold text-slate-400">por month</p>
-
-                  <Button />
-               </div>
-               <button className="text-xs font-bold text-slate-400">
-                  Mostrar menos
-               </button>
-            </article>
-
-            <article className="w-full max-w-xs rounded-md border shadow-rounded">
-               <div className="flex flex-col items-center border-b border-tertiary p-2">
-                  <h3 className="font-bold">Basico</h3>
-                  <p className="font-bold">$0</p>
-                  <p className="text-xs font-bold text-slate-400">por month</p>
-
-                  <Button />
-               </div>
-               <button className="text-xs font-bold text-slate-400">
-                  Mostrar menos
-               </button>
-            </article>
-
-            <div>
-               <h4>Dúvidas frequentes</h4>
-
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               <p className="w-5/6 text-center">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               </p>
             </div>
-
-            <div>
-               <ul>
-                  <li>
-                     1 opção
-                     <p>Lorem ipsum dolor sit.</p>
-                  </li>
-                  <li>
-                     2 opção
-                     <p>Lorem ipsum dolor sit.</p>
-                  </li>
-                  <li>
-                     3 opção
-                     <p>Lorem ipsum dolor sit.</p>
-                  </li>
-                  <li>
-                     4 opção
-                     <p>Lorem ipsum dolor sit.</p>
-                  </li>
-                  <li>
-                     5 opção
-                     <p>Lorem ipsum dolor sit.</p>
-                  </li>
-               </ul>
-            </div>
+            <Faq faqs={faqData} />
          </section>
-         <section className="h-[420px] w-full bg-bg-header bg-contain bg-no-repeat">
-            <footer>Footer é Aqui</footer>
+         <section className="text-white">
+            <Footer />
          </section>
       </div>
    );
