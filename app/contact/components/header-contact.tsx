f1073f2a6d2f4747e4e133a7a8176a2a815d34a1
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-08-21 19:17:17 -0300

    feat: add Header Contact component

---
+++ app/contact/components/header-contact.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import MenuMobile from "@/app/components/menu-mobile";
+import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+
+const HeaderContact = () => {
+   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+   const toggleMenu = () => {
+      setIsOpen(!isOpen);
+   };
+
+   return (
+      <header className="flex h-14 items-center justify-between text-primary">
+         <div>
+            <h1 className="font-semibol pb-3 text-3xl">Contato</h1>
+            <div className="h-1.5 w-20 rounded-md bg-blue-600"></div>
+         </div>
+
+         <button onClick={toggleMenu}>
+            <IoMenu className="size-8" />
+         </button>
+
+         {isOpen && <MenuMobile isOpen={isOpen} toggleMenu={toggleMenu} />}
+      </header>
+   );
+};
+
+export default HeaderContact;
