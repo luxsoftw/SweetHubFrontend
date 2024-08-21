0976700f67d013b57aef2667382da749115acc4f
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-08-21 19:21:43 -0300

    refactor: componentizing contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,75 +1,28 @@
-"use client";
-
-import { useState } from "react";
-import { IoMenu } from "react-icons/io5";
-import MenuMobile from "../components/menu-mobile";
+import HeaderContact from "./components/header-contact";
+import InputFormContact from "./components/input-form-contact";
 
 const Contact = () => {
-   const [isOpen, setIsOpen] = useState<boolean>(false);
-
-   const toggleMenu = () => {
-      setIsOpen(!isOpen);
-   };
-
    return (
       <div className="flex h-screen items-center justify-center bg-[#00559E]">
-         <div className="h-[640px] w-[350px] max-w-md rounded-lg bg-blue-900 p-8 shadow-lg">
-            <section id="contact-header" className="flex justify-between">
-               <div>
-                  <h2 className="text-3xl font-bold text-white">Contato</h2>
-                  <img className="mt-4" src="/active-indicator.png" alt="" />
-               </div>
-               <button className="mb-4" onClick={toggleMenu}>
-                  <IoMenu className="size-8 text-white" />
-               </button>
+         <div className="h-[640px] w-[350px] max-w-md rounded-lg bg-blue-900 px-5 py-2 shadow-lg">
+            <HeaderContact />
 
-               {isOpen && (
-                  <MenuMobile isOpen={isOpen} toggleMenu={toggleMenu} />
-               )}
-            </section>
+            <form className="mt-7 space-y-4">
+               <InputFormContact placeholder="Qual o seu nome? *" />
 
-            <form className="mt-5">
-               <div className="mb-4">
-                  <input
-                     type="text"
-                     placeholder="Qual o seu nome? *"
-                     className="w-full rounded-md border border-white bg-transparent p-2 text-white placeholder:text-white focus:outline-none focus:ring-2 focus:ring-transparent"
-                  />
-               </div>
+               <InputFormContact placeholder="Qual o seu email? *" />
 
-               <div className="mb-4">
-                  <input
-                     type="email"
-                     placeholder="Qual o seu email? *"
-                     className="focus:[#F7A932] w-full rounded-md border border-[#F7A932] bg-transparent p-2 text-[#F7A932] placeholder:text-[#F7A932] focus:outline-none focus:ring-2"
-                  />
-               </div>
+               <InputFormContact placeholder="Qual o seu número de telefone? *" />
 
-               <div className="mb-4">
-                  <input
-                     type="tel"
-                     placeholder="Qual o seu número de telefone? *"
-                     className="focus:[#F7A932] w-full rounded-md border border-[#F7A932] bg-transparent p-2 text-[#F7A932] placeholder:text-[#F7A932] focus:outline-none focus:ring-2"
-                  />
-               </div>
+               <InputFormContact placeholder="Qual nome da sua empresa?" />
 
-               <div className="mb-4">
-                  <input
-                     type="text"
-                     placeholder="Qual nome da sua empresa?"
-                     className="w-full rounded-md border border-white bg-transparent p-2 text-white placeholder:text-white focus:outline-none focus:ring-1 focus:ring-[#F7A932]"
-                  />
-               </div>
+               <textarea
+                  placeholder="Escreva sua mensagem."
+                  className="w-full rounded-md border border-white bg-transparent p-2 text-white placeholder:text-white focus:border-none focus:text-[#F7A932] focus:outline-none focus:ring-2 focus:ring-[#F7A932] focus:placeholder:text-[#F7A932]"
+                  rows={4}
+               ></textarea>
 
-               <div className="mb-4">
-                  <textarea
-                     placeholder="Escreva sua mensagem."
-                     className="w-full rounded-md border border-white bg-transparent p-2 text-white placeholder:text-white focus:outline-none focus:ring-2 focus:ring-transparent"
-                     rows={4}
-                  ></textarea>
-               </div>
-
-               <div className="mb-4 flex items-center">
+               <div className="flex items-center">
                   <input
                      type="checkbox"
                      id="terms"
