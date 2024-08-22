4976e1e1ba6c9ee3c2c21c4469d148917def3817
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-08-21 22:00:09 -0300

    feat: add `Register Informations` page

---
+++ app/register/informations/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { AuthBackground } from "@/app/components/auth-background";
+import { AuthButton } from "@/app/components/auth-button";
+import { HeaderRegister } from "@/app/components/header-register";
+import { Input } from "@/app/components/input-form/index";
+
+const GeneralInformationRegisterPage = () => {
+   return (
+      <AuthBackground>
+         <HeaderRegister title="Registrar" text="Informações Gerais" />
+
+         <Input.Root>
+            <Input.Form type="text" placeholder="CNPJ" />
+            <Input.IconSearch
+               onClick={() => {
+                  console.log("Search for CNPJ");
+               }}
+            />
+         </Input.Root>
+
+         <Input.Root>
+            <Input.Form name="cpf" placeholder="CPF" />
+         </Input.Root>
+
+         <Input.Root>
+            <Input.Form name="corporate-name" placeholder="Razão Social" />
+         </Input.Root>
+
+         <Input.Root>
+            <Input.Form name="brand-name" placeholder="Nome Fantasia" />
+         </Input.Root>
+
+         <AuthButton type="submit" title="Continuar" />
+      </AuthBackground>
+   );
+};
+
+export default GeneralInformationRegisterPage;
