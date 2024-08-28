2fda4b947d29f6ad9ec4a341e5ff8d014ea87c38
Helcio <helcioanicio19@gmail.com>
2024-08-28 14:52:28 -0300

    feat(price): added responsiveness Faq in price screen

--- app/prices/page.tsx
+++ app/prices/page.tsx
@@ -3,13 +3,45 @@ import { IoShieldCheckmark } from "react-icons/io5";
 import Footer from "../components/footer";
 import Faq from "../components/faq";
 import { Card } from "./Components/card";
-import { listfaqs } from "../lib/faqs";
 
 const PricePage = () => {
+   const faqData = [
+      {
+         id: 1,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+      {
+         id: 2,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+      {
+         id: 3,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+      {
+         id: 4,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+      {
+         id: 5,
+         question: "background-image: linear-gradient?",
+         answer:
+            "background-image: linear-gradient(to top, var(--tw-gradient-stops));",
+      },
+   ];
+
    return (
       <div className="font-sans">
          <section className="h-56 w-full bg-wave-header bg-cover bg-bottom bg-no-repeat text-white md:h-72">
-            <Header />
+            <Header color={"text-primary"} />
          </section>
 
          <section className="flex flex-col items-center gap-10 bg-white">
@@ -29,22 +61,24 @@ const PricePage = () => {
                <Card color="white" price={100} plan="Premium" />
             </article>
 
-            <div className="mt-10 flex flex-col items-center gap-10">
-               <h4 className="text-2xl font-medium uppercase text-gray-400">
-                  Dúvidas frequentes
-               </h4>
-               <span className="h-1 w-1/2 bg-amber-400"> </span>
+            <article className="mt-10 flex flex-col items-center gap-10 lg:flex-row lg:items-baseline lg:justify-center">
+               <div className="flex flex-col items-center gap-10">
+                  <h4 className="text-2xl font-medium uppercase text-gray-400">
+                     Dúvidas frequentes
+                  </h4>
+                  <span className="h-1 w-1/2 bg-amber-400"> </span>
 
-               <p className="w-5/6 text-center">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-               </p>
-            </div>
-
-            <Faq faqs={listfaqs} />
+                  <p className="w-5/6 text-center">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+               </div>
+               <Faq faqs={faqData} />
+            </article>
          </section>
-
-         <Footer />
-      </section>
+         <section className="text-white lg:text-black">
+            <Footer />
+         </section>
+      </div>
    );
 };
 
