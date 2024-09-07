c7720943f951c855f4aed559a4516c76419123a7
MateusOliveira30 <mcesaroliveira30@yahoo.com>
2024-09-06 21:53:02 -0300

    fix: responsiveContact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -47,41 +47,40 @@ const Contact = () => {
                      className="col-span-2 w-full rounded-md border border-white bg-transparent p-4 text-white placeholder:text-white focus:border-none focus:text-[#F7A932] focus:outline-none focus:ring-2 focus:ring-[#F7A932] focus:placeholder:text-[#F7A932]"
                      rows={4}
                   ></textarea>
+
+                  <section className="col-span-2 flex flex-col gap-3 pt-10 md:flex-row md:items-center md:justify-around">
+                     <div className="flex items-center">
+                        <input
+                           type="checkbox"
+                           id="terms"
+                           className="ml-3 mr-3 h-6 w-6 accent-[#F7A932] md:mr-4"
+                        />
+                        <label
+                           htmlFor="terms"
+                           className="font-normal text-white md:text-[20px]"
+                        >
+                           Li e aceito os Termos de Serviço e a{" "}
+                           <span className="text-[#F7A932]">
+                              Política de Privacidade *
+                           </span>
+                        </label>
+                     </div>
+
+                     <button
+                        type="submit"
+                        className="mt-0 hidden h-[50px] w-[250px] items-center justify-center rounded-3xl bg-[#F7A932] text-center font-normal text-white hover:bg-yellow-500 md:flex"
+                     >
+                        ENVIAR MENSAGEM
+                     </button>
+
+                     <button
+                        type="submit"
+                        className="mx-auto mt-3 flex h-[45px] w-full items-center justify-center rounded-md bg-[#F7A932] px-4 text-center font-normal text-white hover:bg-yellow-500 md:hidden"
+                     >
+                        ENVIAR
+                     </button>
+                  </section>
                </form>
-            </section>
-            <section className="flex flex-col pt-10 md:flex-row md:justify-around">
-               <div className="flex">
-                  <div className="flex items-center">
-                     <input
-                        type="checkbox"
-                        id="terms"
-                        className="ml-3 mr-3 h-6 w-6 accent-[#F7A932] md:mr-8"
-                     />
-                  </div>
-                  <label
-                     htmlFor="terms"
-                     className="font-normal text-white md:text-[20px]"
-                  >
-                     Li e aceito os Termos de Serviço e a{" "}
-                     <span className="text-[#F7A932]">
-                        Política de Privacidade *
-                     </span>
-                  </label>
-               </div>
-
-               <button
-                  type="submit"
-                  className="mt-0 hidden h-[50px] w-[250px] items-center justify-center rounded-3xl bg-[#F7A932] text-center font-normal text-white hover:bg-yellow-500 md:flex"
-               >
-                  ENVIAR MENSAGEM
-               </button>
-
-               <button
-                  type="submit"
-                  className="mx-auto mt-3 flex h-[45px] w-full items-center justify-center rounded-md bg-[#F7A932] px-4 text-center font-normal text-white hover:bg-yellow-500 md:hidden"
-               >
-                  ENVIAR
-               </button>
             </section>
          </div>
       </div>
