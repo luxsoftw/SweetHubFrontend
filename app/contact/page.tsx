521b90db21011abd8a8ebc98c86329df485863aa
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-09-09 15:21:58 -0300

    chore: add React Hook Form and Use Mask Input

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -32,17 +32,22 @@ const Contact = () => {
             <section className="w-full">
                <form className="mt-7 flex w-full grid-cols-1 flex-col gap-y-4 md:grid md:grid-cols-2 md:gap-4 md:gap-y-10">
                   <InputFormContact placeholder="Qual seu nome? *" />
+
                   <InputFormContact placeholder="Qual seu e-mail? *" />
+
                   <InputFormContact
                      type="tel"
                      placeholder="Qual seu telefone?"
                   />
+
                   <InputFormContact placeholder="Qual é a sua empresa?" />
+
                   <textarea
                      placeholder="Escreva sua mensagem aqui"
                      className="col-span-2 w-full rounded-md border border-white bg-transparent p-4 text-white placeholder:text-white focus:border-none focus:text-[#F7A932] focus:outline-none focus:ring-2 focus:ring-[#F7A932] focus:placeholder:text-[#F7A932]"
                      rows={4}
                   ></textarea>
+
                   <section className="col-span-2 flex flex-col gap-3 pt-10 md:flex-row md:items-center md:justify-around">
                      <div className="flex items-center">
                         <input
@@ -50,6 +55,7 @@ const Contact = () => {
                            id="terms"
                            className="ml-3 mr-3 h-6 w-6 accent-[#F7A932] md:mr-4"
                         />
+
                         <label
                            htmlFor="terms"
                            className="font-normal text-white md:text-[20px]"
@@ -60,12 +66,14 @@ const Contact = () => {
                            </span>
                         </label>
                      </div>
+
                      <button
                         type="submit"
                         className="mt-0 hidden h-[50px] w-[250px] items-center justify-center rounded-3xl bg-[#F7A932] text-center font-normal text-white hover:bg-yellow-500 md:flex"
                      >
                         ENVIAR MENSAGEM
                      </button>
+
                      <button
                         type="submit"
                         className="mx-auto mt-3 flex h-[45px] w-full items-center justify-center rounded-md bg-[#F7A932] px-4 text-center font-normal text-white hover:bg-yellow-500 md:hidden"
