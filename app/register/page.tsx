c3866059d26c227cf8cf3dd2eff6840adcef9211
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-09-12 12:43:16 -0300

    feat: add Register page form validation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { CheckBoxTerm } from "./components/checkbox-term";
 import { Input } from "../components/input-form/index";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -8,6 +7,13 @@ import { AuthLayout } from "../components/auth-layout";
 import { AuthLeftSide } from "../components/auth-left-side";
 import { AuthRightSide } from "../components/auth-right-side";
 import { AuthButton } from "../components/auth-button";
+import { useForm } from "react-hook-form";
+import validator from "validator";
+import { useHookFormMask } from "use-mask-input";
+import InvolveInputError from "../components/involve-input-error";
+import InputErrorMessage from "../components/input-error-message";
+import RegisterFormType from "../types/register-form";
+import { CheckBoxTerm } from "../components/checkbox-term";
 
 const RegisterPage = () => {
    const router = useRouter();
@@ -21,6 +27,19 @@ const RegisterPage = () => {
       setIsOpen(!isOpen);
    };
 
+   const {
+      register,
+      getValues,
+      handleSubmit,
+      formState: { errors },
+   } = useForm<RegisterFormType>();
+
+   const registerWithMask = useHookFormMask(register);
+
+   const handleRegister = (data: RegisterFormType) => {
+      console.log(data);
+   };
+
    return (
       <AuthLayout>
          <AuthLeftSide
@@ -31,10 +50,7 @@ const RegisterPage = () => {
          <AuthRightSide isOpen={isOpen} handleToggleMenu={handleToggleMenu}>
             <form
                className="mt-4 flex w-full flex-grow flex-col space-y-8 md:mt-8"
-               onSubmit={(e) => {
-                  e.preventDefault();
-                  console.log("Form submitted");
-               }}
+               onSubmit={handleSubmit(handleRegister)}
             >
                <div className="flex flex-row items-center justify-center gap-5">
                   <div className="h-0.5 w-14 rounded bg-black/30 md:h-[0.080rem] md:w-20"></div>
@@ -50,45 +66,171 @@ const RegisterPage = () => {
                   Já possui conta? Entre aqui!
                </a>
 
-               <Input.Root>
-                  <Input.Form type="text" placeholder="nome" required />
-               </Input.Root>
+               <div className="relative">
+                  <Input.Root>
+                     <Input.Form
+                        {...register("name", {
+                           required: true,
+                           maxLength: 20,
+                           minLength: 3,
+                        })}
+                        type="text"
+                        placeholder="Nome"
+                     />
+                  </Input.Root>
 
-               <Input.Root>
-                  <Input.Form type="email" placeholder="E-mail" required />
-               </Input.Root>
+                  <InvolveInputError>
+                     {errors.name?.type === "required" && (
+                        <InputErrorMessage>
+                           Nome é obrigatório
+                        </InputErrorMessage>
+                     )}
 
-               <Input.Root>
-                  <Input.Form
-                     type="tel"
-                     placeholder="Número"
-                     pattern="[0-9]{10,11}"
-                     title="Please enter a valid phone number (10-11 digits)"
-                     required
-                  />
-               </Input.Root>
+                     {errors.name?.type === "minLength" && (
+                        <InputErrorMessage>
+                           Nome deve ter no mínimo 3 caracteres
+                        </InputErrorMessage>
+                     )}
+
+                     {errors.name?.type === "maxLength" && (
+                        <InputErrorMessage>
+                           Nome deve ter no máximo 20 caracteres
+                        </InputErrorMessage>
+                     )}
+                  </InvolveInputError>
+               </div>
+
+               <div className="relative">
+                  <Input.Root>
+                     <Input.Form
+                        {...register("email", {
+                           required: true,
+                           validate: (value) => {
+                              return validator.isEmail(value);
+                           },
+                        })}
+                        type="email"
+                        placeholder="E-mail"
+                     />
+                  </Input.Root>
+
+                  <InvolveInputError>
+                     {errors.email?.type === "required" && (
+                        <InputErrorMessage>
+                           Email é obrigatório
+                        </InputErrorMessage>
+                     )}
+
+                     {errors.email?.type === "validate" && (
+                        <InputErrorMessage>Email inválido</InputErrorMessage>
+                     )}
+                  </InvolveInputError>
+               </div>
+
+               <div className="relative">
+                  <Input.Root>
+                     <Input.Form
+                        {...registerWithMask("phone", "(99) 99999-9999", {
+                           required: true,
+                           validate: (value) => {
+                              return validator.isMobilePhone(value, "pt-BR");
+                           },
+                        })}
+                        type="tel"
+                        placeholder="Número"
+                        title="Please enter a valid phone number (10-11 digits)"
+                     />
+                  </Input.Root>
+
+                  <InvolveInputError>
+                     {errors.phone?.type === "required" && (
+                        <InputErrorMessage>
+                           Número de telefone é obrigatório
+                        </InputErrorMessage>
+                     )}
+
+                     {errors.phone?.type === "validate" && (
+                        <InputErrorMessage>
+                           Número de telefone inválido
+                        </InputErrorMessage>
+                     )}
+                  </InvolveInputError>
+               </div>
 
                <div className="flex gap-4">
-                  <label className="flex w-1/2 flex-row items-center rounded-md border border-black/40 px-2 ring-[#F7A932] ring-offset-2 focus-within:border-none focus-within:ring-1">
-                     <input
-                        type="password"
-                        placeholder="Senha"
-                        required
-                        className="h-10 w-28 rounded-md bg-transparent px-5 outline-none"
-                     />
-                  </label>
-                  <label className="flex w-1/2 flex-row items-center rounded-md border border-black/40 px-2 ring-[#F7A932] ring-offset-2 focus-within:border-none focus-within:ring-1">
-                     <input
-                        type="password"
-                        placeholder="Repita a senha"
-                        required
-                        className="flex h-10 w-28 flex-grow rounded-md bg-transparent px-5 outline-none"
-                     />
-                  </label>
+                  <div className="relative w-full">
+                     <Input.Root>
+                        <input
+                           {...register("password", {
+                              required: true,
+                              minLength: 8,
+                           })}
+                           type="password"
+                           placeholder="Senha"
+                           className="h-10 w-28 rounded-md bg-transparent px-5 outline-none"
+                        />
+                     </Input.Root>
+
+                     <InvolveInputError>
+                        {errors.password?.type === "required" && (
+                           <InputErrorMessage>
+                              Senha é obrigatória
+                           </InputErrorMessage>
+                        )}
+
+                        {errors.password?.type === "minLength" && (
+                           <InputErrorMessage>
+                              Senha deve ter no mínimo 8 caracteres
+                           </InputErrorMessage>
+                        )}
+                     </InvolveInputError>
+                  </div>
+
+                  <div className="relative w-full">
+                     <Input.Root>
+                        <input
+                           {...register("confirmPassword", {
+                              required: true,
+                              validate: (value) => {
+                                 return value === getValues("password");
+                              },
+                           })}
+                           type="password"
+                           placeholder="Repita a senha"
+                           className="flex h-10 w-28 flex-grow rounded-md bg-transparent px-5 outline-none"
+                        />
+                     </Input.Root>
+
+                     <InvolveInputError>
+                        {errors.confirmPassword?.type === "required" && (
+                           <InputErrorMessage>
+                              Confirmação de senha é obrigatória
+                           </InputErrorMessage>
+                        )}
+
+                        {errors.confirmPassword?.type === "validate" && (
+                           <InputErrorMessage>
+                              Senhas não coincidem
+                           </InputErrorMessage>
+                        )}
+                     </InvolveInputError>
+                  </div>
                </div>
 
                <div className="flex flex-col gap-10 md:flex-row md:items-center md:justify-between md:gap-2">
-                  <CheckBoxTerm text="Aceito receber contato no Whatsapp" />
+                  <div className="relative">
+                     <CheckBoxTerm {...register("terms", { required: true })}>
+                        Aceito os termos de uso
+                     </CheckBoxTerm>
+
+                     <InvolveInputError>
+                        {errors.terms?.type === "required" && (
+                           <InputErrorMessage>
+                              Aceitar os termos é obrigatório
+                           </InputErrorMessage>
+                        )}
+                     </InvolveInputError>
+                  </div>
 
                   <AuthButton title="CONTINUAR" />
                </div>
