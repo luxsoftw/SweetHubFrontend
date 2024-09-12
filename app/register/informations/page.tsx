4f8666462606e52573eedbe05847cef997d0240c
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-09-12 20:07:48 -0300

    feat: add Informations page form validation

--- app/register/informations/page.tsx
+++ app/register/informations/page.tsx
@@ -4,19 +4,18 @@ import { AuthButton } from "@/app/components/auth-button";
 import { AuthLayout } from "@/app/components/auth-layout";
 import { AuthLeftSide } from "@/app/components/auth-left-side";
 import { AuthRightSide } from "@/app/components/auth-right-side";
+import InputErrorMessage from "@/app/components/input-error-message";
 import { Input } from "@/app/components/input-form/index";
+import InvolveInputError from "@/app/components/involve-input-error";
+import InformationsFormType from "@/app/types/informations-form";
 import { useRouter } from "next/navigation";
-import type { FormEvent } from "react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useHookFormMask } from "use-mask-input";
 
 const GeneralInformationRegisterPage = () => {
    const router = useRouter();
    const [isOpen, setIsOpen] = useState(false);
-
-   function handleGoTo(event: FormEvent) {
-      event.preventDefault();
-      router.push("/register/address");
-   }
 
    const handleClose = () => {
       router.back();
@@ -24,6 +23,18 @@ const GeneralInformationRegisterPage = () => {
 
    const handleToggleMenu = () => {
       setIsOpen(!isOpen);
+   };
+
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm<InformationsFormType>();
+
+   const registerWithMask = useHookFormMask(register);
+
+   const handleInformations = (data: InformationsFormType) => {
+      console.log(data);
    };
 
    return (
@@ -35,7 +46,7 @@ const GeneralInformationRegisterPage = () => {
          />
          <AuthRightSide isOpen={isOpen} handleToggleMenu={handleToggleMenu}>
             <form
-               onSubmit={handleGoTo}
+               onSubmit={handleSubmit(handleInformations)}
                className="flex flex-col items-center gap-10"
             >
                <div className="flex flex-row items-center justify-center gap-5">
@@ -45,33 +56,137 @@ const GeneralInformationRegisterPage = () => {
                   </h4>
                   <div className="h-0.5 w-14 rounded bg-black/30 md:h-[0.080rem] md:w-20"></div>
                </div>
-               <Input.Root>
-                  <Input.Form
-                     type="text"
-                     placeholder="CNPJ"
-                     className="flex-grow"
-                  />
-                  <Input.IconSearch
-                     onClick={() => {
-                        console.log("Search for CNPJ");
-                     }}
-                  />
-               </Input.Root>
+               <div className="relative w-full">
+                  <Input.Root>
+                     <Input.Form
+                        {...registerWithMask("cnpj", "cnpj", {
+                           required: true,
+                           pattern: {
+                              value: /^\d{2}.\d{3}.\d{3}\/\d{4}-\d{2}$/,
+                              message: "CNPJ inválido",
+                           },
+                        })}
+                        type="text"
+                        placeholder="CNPJ"
+                        className="flex-grow"
+                     />
+                     <Input.IconSearch
+                        onClick={() => {
+                           console.log("Search for CNPJ");
+                        }}
+                     />
+                  </Input.Root>
 
-               <Input.Root>
-                  <Input.Form name="cpf" placeholder="CPF" />
-               </Input.Root>
+                  <InvolveInputError>
+                     {errors.cnpj?.type === "required" && (
+                        <InputErrorMessage>
+                           CNPJ é obrigatório
+                        </InputErrorMessage>
+                     )}
 
-               <Input.Root>
-                  <Input.Form
-                     name="corporate-name"
-                     placeholder="Razão Social"
-                  />
-               </Input.Root>
+                     {errors.cnpj?.type === "pattern" && (
+                        <InputErrorMessage>
+                           {String(errors.cnpj.message)}
+                        </InputErrorMessage>
+                     )}
+                  </InvolveInputError>
+               </div>
 
-               <Input.Root>
-                  <Input.Form name="brand-name" placeholder="Nome Fantasia" />
-               </Input.Root>
+               <div className="relative w-full">
+                  <Input.Root>
+                     <Input.Form
+                        {...registerWithMask("cpf", "cpf", {
+                           required: true,
+                           pattern: {
+                              value: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
+                              message: "CPF inválido",
+                           },
+                        })}
+                        name="cpf"
+                        placeholder="CPF"
+                     />
+                  </Input.Root>
+
+                  <InvolveInputError>
+                     {errors.cpf?.type === "required" && (
+                        <InputErrorMessage>CPF é obrigatório</InputErrorMessage>
+                     )}
+
+                     {errors.cpf?.type === "pattern" && (
+                        <InputErrorMessage>
+                           {String(errors.cpf.message)}
+                        </InputErrorMessage>
+                     )}
+                  </InvolveInputError>
+               </div>
+
+               <div className="relative w-full">
+                  <Input.Root>
+                     <Input.Form
+                        {...register("corporateName", {
+                           required: true,
+                           maxLength: 20,
+                           minLength: 2,
+                        })}
+                        name="corporateName"
+                        placeholder="Razão Social"
+                     />
+                  </Input.Root>
+
+                  <InvolveInputError>
+                     {errors.corporateName?.type === "required" && (
+                        <InputErrorMessage>
+                           Razão Social é obrigatório
+                        </InputErrorMessage>
+                     )}
+
+                     {errors.corporateName?.type === "minLength" && (
+                        <InputErrorMessage>
+                           Razão Social deve ter no mínimo 2 caracteres
+                        </InputErrorMessage>
+                     )}
+
+                     {errors.corporateName?.type === "maxLength" && (
+                        <InputErrorMessage>
+                           Razão Social deve ter no máximo 20 caracteres
+                        </InputErrorMessage>
+                     )}
+                  </InvolveInputError>
+               </div>
+
+               <div className="relative w-full">
+                  <Input.Root>
+                     <Input.Form
+                        {...register("fantasyName", {
+                           required: true,
+                           maxLength: 20,
+                           minLength: 2,
+                        })}
+                        name="fantasyName"
+                        placeholder="Nome Fantasia"
+                     />
+                  </Input.Root>
+
+                  <InvolveInputError>
+                     {errors.fantasyName?.type === "required" && (
+                        <InputErrorMessage>
+                           Nome Fantasia é obrigatório
+                        </InputErrorMessage>
+                     )}
+
+                     {errors.fantasyName?.type === "minLength" && (
+                        <InputErrorMessage>
+                           Nome Fantasia deve ter no mínimo 2 caracteres
+                        </InputErrorMessage>
+                     )}
+
+                     {errors.fantasyName?.type === "maxLength" && (
+                        <InputErrorMessage>
+                           Nome Fantasia deve ter no máximo 20 caracteres
+                        </InputErrorMessage>
+                     )}
+                  </InvolveInputError>
+               </div>
 
                <AuthButton title="CONTINUAR" />
             </form>
