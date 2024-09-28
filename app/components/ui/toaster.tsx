f5a81271909a7f67c64839aabe93f6619e1bb9c0
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-09-28 17:56:49 -0300

    fix: fix path of lib utils in shadcn components

--- app/components/ui/toaster.tsx
+++ app/components/ui/toaster.tsx
@@ -8,7 +8,7 @@ import {
    ToastProvider,
    ToastTitle,
    ToastViewport,
-} from "@/components/ui/toast";
+} from "@/app/components/ui/toast";
 
 export function Toaster() {
    const { toasts } = useToast();
