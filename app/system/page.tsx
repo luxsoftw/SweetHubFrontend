b9ca7484c0f1d1726325fd2415a8460d94e988a9
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-10-13 11:06:29 -0300

    refactor: remove system `page.tsx`
    page in system path is unecessary

--- app/system/page.tsx
+++
@@ -1,9 +0,0 @@
-function SystemPage() {
-   return (
-      <div className="flex h-screen items-center justify-center">
-         <p>System Page</p>
-      </div>
-   );
-}
-
-export default SystemPage;
