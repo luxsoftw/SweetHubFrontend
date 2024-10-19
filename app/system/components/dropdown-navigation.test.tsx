d7553bb3b2d9d7b54d9035ce827b76ce73f02f3e
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-10-19 13:15:33 -0300

    fix: add mock pathname to test

--- app/system/components/dropdown-navigation.test.tsx
+++ app/system/components/dropdown-navigation.test.tsx
@@ -2,18 +2,21 @@ import { render } from "@testing-library/react";
 import { DropdownNavigation, DropDownItem } from "./dropdown-navigation";
 import { PiSquaresFourLight } from "react-icons/pi";
 
+// Mock the usePathname function from next/navigation
+vi.mock("next/navigation", () => ({
+   usePathname: () => "/system/pricing",
+}));
+
 describe("Dropdown Navigation", () => {
    it("should be have Pricing title", () => {
       const wrapper = render(
-         <DropdownNavigation title="Test" icon={PiSquaresFourLight}>
+         <DropdownNavigation title="Pricing" icon={PiSquaresFourLight}>
             <DropDownItem href="/system/pricing">Pricing</DropDownItem>
          </DropdownNavigation>,
       );
 
       const text = "Pricing";
       const statusText = wrapper.getByText(text);
-      console.log(statusText.outerHTML);
-      wrapper.debug();
       expect(statusText).toBeInTheDocument();
    });
 });
